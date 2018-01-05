64bc7c9426cb8678e5b1c7ad207398adf04d98d7
halil9 <halilbozan9@gmail.com>
2018-01-05 18:43:47 +0300

    code devoloped.

--- splitt_data.py
+++ splitt_data.py
@@ -3,29 +3,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from collections import Counter
 from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+
 
 svm = SVC()
 cnt = Counter()
 data = pd.read_csv("test_data.csv")
-main_attribute = ["credit_policy","purpose","int.rate","installment","log.annual.inc","dti","fico","days.with.cr.line","revol.bal","revol.util","inq.last.6mths","delinq.2yrs","pub.rec","paid_stat"]
-datas = pd.DataFrame(data,columns=main_attribute)
+main_attribute = ["credit_policy", "purpose","int.rate", "installment", "log.annual.inc", "dti","fico", "days.with.cr.line", "revol.bal", "revol.util", "inq.last.6mths", "delinq.2yrs", "pub.rec", "paid_stat"]
+datas = pd.DataFrame(data, columns=main_attribute)
 
-for  i in datas['purpose']:
+for i in datas['purpose']:
     cnt[i] += 1
-k=0
-t={}
+k = 0
+t = {}
 for i in cnt.keys():
-    t[i]=k
-k+=1
+    t[i] = k
+k += 1
 
-datas['purpose']=datas.purpose.map(t)
-main_data= datas[main_attribute]
-real= datas["paid_stat"]
-X_train, X_test, y_train, y_test = train_test_split(main_data, real, test_size=1)
+datas['purpose'] = datas.purpose.map(t)
+main_data = datas[main_attribute]
+real = datas["paid_stat"]
+X_train, X_test, y_train, y_test = train_test_split(main_data, real, test_size=0.5)
 print(X_train.shape, y_train.shape)
 print(X_test.shape, y_test.shape)
 
-svm.fit(X_train,y_train)
-print (X_train)
-predictions=svm.predict(X_test)
-print( accuracy_score(y_test,predictions))
+svm.fit(X_train, y_train)
+print(X_train)
+predictions = svm.predict(X_test)
+print(accuracy_score(predictions, y_test))
+
+predict_status = datas.loc[datas['purpose'] == t['credit_card']]
+
+predict_status1 = predict_status.loc[predict_status['paid_stat'] == 1]
+predict_status2 = predict_status.loc[predict_status['paid_stat'] == 0]
+
+
+predict_status.hist()
+plt.show()
